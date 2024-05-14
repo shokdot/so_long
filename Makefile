ec88f125ee1b1252e2299665bd487dd370efac85
Henrik Aleksanyan <healeksa@c2r11s6.42yerevan.am>
2024-05-14 22:33:40 +0400

    added dependecies of mlx

--- Makefile
+++ Makefile
@@ -7,15 +7,19 @@ INCLUDES = $(wildcard includes/*.h)
 OBJ_DIR = obj
 OBJ = $(patsubst %.c, $(OBJ_DIR)/%.o, $(SRC))
 CFLAGS = -I # -Wall -Wextra -Werror -I
+MLX = mlx
 RM = rm -f
 
-all: $(OBJ_DIR) $(NAME)
+all: $(MLX) $(NAME)
+
+$(MLX): $(OBJ_DIR)
+	make -C $(MLX)
 
 $(OBJ_DIR):
 	@mkdir -p $(OBJ_DIR)
 
 $(NAME): $(OBJ) Makefile $(INCLUDES)
-	@$(CC) $(CFLAGS) $(INCLUDES) $(OBJ) -o $(NAME)
+	@$(CC) $(CFLAGS) $(INCLUDES) $(OBJ) -o $(NAME) -L mlx -l mlx -framework OpenGL -framework AppKit
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c Makefile $(INCLUDES)
 	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
@@ -31,5 +35,3 @@ fclean: clean
 	@$(RM) $(NAME)
 
 re: fclean all
-
-
--- src/main.c
+++ src/main.c
@@ -6,10 +6,18 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 18:50:23 by healeksa          #+#    #+#             */
-/*   Updated: 2024/05/14 20:49:55 by healeksa         ###   ########.fr       */
+/*   Updated: 2024/05/14 22:00:56 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "mlx.h"
+
 int	main(int argc, char **argv)
 {
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 }
